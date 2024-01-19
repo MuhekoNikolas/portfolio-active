f5f856ddc0b369e5357f2b360ca30d2766d8bb4f
Muheko Nikolas <13nikolusmuheko@gmail.com>
2024-01-19 22:54:20 +0100

    Modified cache.js file

--- backend/config.loader.js
+++ backend/config.loader.js
@@ -1,5 +1,6 @@
 
 import iframelyConfig from './config.js';
+import path from 'path'
 
 import * as fs from 'fs';
 
@@ -16,8 +17,13 @@ if(process.cwd().endsWith("portfolio") == true){
 
 //var globalConfig = import(`${globalConfigPath}`);
 
-var globalConfig = import(`file:///${process.cwd()}/config.js`);  //`./config.js`);
+var currentModuleURL = new URL(import.meta.url);
+var currentModulePath = path.dirname(currentModuleURL.pathname);
+var _path = `${currentModulePath}/config.js`//portfolio/backendResources/config.cjs`
+var globalConfig = await import(_path) 
+
 globalConfig = globalConfig && globalConfig.default;
 
+//console.log(iframelyConfig, globalConfig, "pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp")
 
 export default {...iframelyConfig, ...globalConfig};
